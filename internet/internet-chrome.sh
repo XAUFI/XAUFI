fd5cd3a31a59270ab340312c29db75ab67df2b5f
dbautistav <dbautistav@gmail.com>
2014-12-23 22:09:44 -0600

    Google Chrome script added

---
+++ internet/internet-chrome.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo ' > Internet :: Google Chrome'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt-get update
+apt-get -y install google-chrome-stable
+echo '   [chrome-internet]     ...done!'
