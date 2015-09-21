4f21c975e473a92e7f0fb4c09b3eec9073f90298
David Bautista <dbautistav@users.noreply.github.com>
2015-09-21 12:26:18 -0500

    Update dev-tools.sh

--- development/dev-tools.sh
+++ development/dev-tools.sh
@@ -4,8 +4,8 @@ echo ' > Development :: Zsh // OhMyZsh // base16 // Z'
 sudo apt-get install -y zsh
 sudo apt-get install -y git-core
 
-#wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-#curl -L http://install.ohmyz.sh | sh
-#sudo chsh -s `which zsh`
-sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+sudo chsh -s `which zsh`
+
 echo '   [git-tools]     ...done!'
