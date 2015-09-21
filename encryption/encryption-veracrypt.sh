62f3978c1be5d32b3b75749ffeac7fb51d1d9e52
David Bautista <dbautistav@users.noreply.github.com>
2015-09-21 11:41:20 -0500

    Bump version

--- encryption/encryption-veracrypt.sh
+++ encryption/encryption-veracrypt.sh
@@ -2,11 +2,11 @@
 echo ' > Encryption :: Veracrypt'
 mkdir veracrypt
 cd veracrypt
-wget 'sourceforge.net/projects/veracrypt/files/VeraCrypt 1.0e/veracrypt-1.0e-setup.tar.bz2'
-tar -xjvf 'veracrypt-1.0e-setup.tar.bz2'
-rm veracrypt-1.0e-setup.tar.bz2
-#./veracrypt-1.0e-setup-gui-x32
-./veracrypt-1.0e-setup-gui-x64
+wget 'sourceforge.net/projects/veracrypt/files/VeraCrypt 1.14/veracrypt-1.14-setup.tar.bz2'
+tar -xjvf 'veracrypt-1.14-setup.tar.bz2'
+rm veracrypt-1.14-setup.tar.bz2
+#./veracrypt-1.14-setup-gui-x32
+./veracrypt-1.14-setup-gui-x64
 cd ..
 rm -rf veracrypt
 echo '   [veracrypt-encryption]     ...done!'
