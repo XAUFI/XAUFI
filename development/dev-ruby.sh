635e9ad5f7d78f4912f4de34fe93a6e595850d95
David Bautista <dbautistav@users.noreply.github.com>
2015-09-21 12:11:50 -0500

    Update dev-ruby.sh

--- development/dev-ruby.sh
+++ development/dev-ruby.sh
@@ -4,14 +4,18 @@ sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libr
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc
-exec $SHELL
+source ~/.zshrc
+#exec $SHELL
 
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.zshrc
-exec $SHELL
+source ~/.zshrc
+#exec $SHELL
 
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
 
