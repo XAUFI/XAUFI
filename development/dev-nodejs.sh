70df0c894d90f60e14afe85b7d9a4d0772e0178e
David Bautista <dbautistav@users.noreply.github.com>
2015-09-21 12:18:12 -0500

    Update dev-nodejs.sh

--- development/dev-nodejs.sh
+++ development/dev-nodejs.sh
@@ -5,4 +5,7 @@ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | 
 nvm install stable
 nvm use stable
 nvm ls
+
+echo 'source ~/.nvm/nvm.sh' >> ~/.bashrc
+echo 'source ~/.nvm/nvm.sh' >> ~/.zshrc
 echo '   [nodejs-dev]     ...done!'
