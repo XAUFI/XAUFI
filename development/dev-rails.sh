2a27831e2eceba71726c5f52ae8b760fbd8038d2
David Bautista <dbautistav@users.noreply.github.com>
2015-09-21 11:56:51 -0500

    b

--- development/dev-rails.sh
+++ development/dev-rails.sh
@@ -3,7 +3,7 @@ echo ' > Development :: Rails'
 ## Uncomment following line if no nodejs installation is present in your system
 #./dev-nodejs.sh
 
-gem install rails -v 4.2.0
+gem install rails -v 4.2.4
 rbenv rehash
 rails -v
 echo '   [rails-dev]     ...done!'
