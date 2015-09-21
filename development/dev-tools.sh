198fa245f38270d8e030e4825cd3582af34f20c1
David Bautista <dbautistav@users.noreply.github.com>
2015-09-21 12:46:04 -0500

    Update dev-tools.sh

--- development/dev-tools.sh
+++ development/dev-tools.sh
@@ -6,6 +6,6 @@ sudo apt-get install -y git-core
 
 #sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-sudo chsh -s `which zsh`
+chsh -s `which zsh`
 
 echo '   [git-tools]     ...done!'
