ca0f0e58ba036dc576253c0afd667d78c7c87155
David Bautista <dbautistav@users.noreply.github.com>
2016-01-20 22:59:00 -0600

    Removing 'entertainment' stuff

--- entertainment/fun-popcorn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo ' > Entertainment :: Popcorn Time'
-add-apt-repository -y ppa:webupd8team/popcorntime
-sudo apt-get update
-sudo apt-get install -y popcorn-time
-echo '   [popcorn-time-fun]     ...done!'
