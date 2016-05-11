46055891300b3164057cab068f3b90dd1b2be4d3
David Bautista <dbautistav@users.noreply.github.com>
2016-05-11 17:36:43 -0500

    Update dev-git.sh

--- development/dev-git.sh
+++ development/dev-git.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo ' > Development :: Git'
-sudo apt-get install -y git
+sudo apt-get install -y git vim
+git config --global core.editor "vim"
+git config --global push.default simple
 git config --global color.ui true
 sudo chown $USER .gitconfig
 echo '   [git-dev]     ...done!'
